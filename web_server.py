05acb12d3a888d82d1ed25262372079f426e7393
Kali Linux <kali@kali>
2026-09-09 21:26:48 +0500

    Add dummy web server for Render port binding

---
+++ web_server.py
@@ -0,0 +1,14 @@
+import os
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot is alive!")
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get('/', handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8080))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
